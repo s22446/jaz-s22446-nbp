e9490b9d4f6e7a1845d3b22a70c37998597f43bd
Michał Siwek <s22446@pjwstk.edu.pl>
2022-06-18 13:40:16 +0200

    Documentation fixes

--- src/main/java/pl/pjatk/jazs22446nbp/golder/model/GolderResponse.java
+++ src/main/java/pl/pjatk/jazs22446nbp/golder/model/GolderResponse.java
@@ -3,7 +3,10 @@ package pl.pjatk.jazs22446nbp.golder.model;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
-@ApiModel(value = "Golder Response", description = "Entity of provided dates and data fetched from NBP API")
+@ApiModel(
+        value = "Golder Response",
+        description = "Response with data and calculated average exchange rate fetched from NBP API"
+)
 public class GolderResponse {
     @ApiModelProperty(
             notes = "Value of average gold exchange rate between inDate and outDate period (for 1g in 1000 sample)",
