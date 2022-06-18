b312411b5d0da43d57c060b405ad9a86f6a1b400
Michał Siwek <s22446@pjwstk.edu.pl>
2022-06-18 11:50:32 +0200

    Project initialization

---
+++ src/main/java/pl/pjatk/jazs22446nbp/JazS22446NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs22446nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS22446NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS22446NbpApplication.class, args);
+	}
+
+}
